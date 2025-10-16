01fc37f8547629030127973b1ace83645570b502
Raymond Finzel <prometheoid@gmail.com>
2025-10-15 23:50:15 -0500

    doc strings and stub out new route

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
+"""
+Hopefully a single file server.
+"""
 from contextlib import asynccontextmanager
 
 from starlette.applications import Starlette
 from starlette.responses import PlainTextResponse
 from starlette.routing import Route
 
+from pygit2 import Repository
+
 @asynccontextmanager
 async def lifespan(app):
+    """
+    Lifespan management.
+    """
     print('Startup')
     yield
     print('Shutdown')
 
 def home(request):
+    """
+    Site index route.
+    """
     return PlainTextResponse('Hello, world!')
 
-routes = [
-    Route('/', home)
+def obj(request):
+    pass
 
+routes = [
+    Route('/', home),
+    Route('/{repo}/{object}', obj)
 ]
 
 app = Starlette(debug=True, routes=routes, lifespan=lifespan)
