1293992df134a7b0beca7c42a6238c22bf07493e
Raymond Finzel <prometheoid@gmail.com>
2025-10-17 12:17:28 -0500

    static file server for index routes

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ from pathlib import Path
 
 from starlette.applications import Starlette
 from starlette.responses import PlainTextResponse
-from starlette.routing import Route
+from starlette.routing import Route, Mount
+from starlette.staticfiles import StaticFiles
 
 from pygit2 import Repository
 from pygit2.enums import SortMode
 
 REPO_HOME = Path(os.environ.get("HYPERHYPER_REPO_HOME", "."))
+SRV_HOME = Path(os.environ.get("HYPERHYPER_SRV_HOME", REPO_HOME))
 
 @asynccontextmanager
 async def lifespan(app):
@@ -24,15 +26,15 @@ async def lifespan(app):
     yield
     print('Shutdown')
 
-def home(request):
+def tree(request):
     """
-    Site index route.
+    Site tree
     """
     repo = Repository(REPO_HOME)
     res = ""
     for commit in repo.walk(repo.head.target, SortMode.TOPOLOGICAL | SortMode.TIME):
         res += "{} | {} | {}\n".format(datetime.datetime.fromtimestamp(commit.commit_time), commit.id, commit.message.rstrip())
-        pass
+
     return PlainTextResponse(res)
 
 def obj(request):
@@ -43,10 +45,11 @@ def obj(request):
     oid = request.path_params['object']
     return PlainTextResponse(repo.get(oid).data)
 
-
+# RESERVED ROUTES: tree, object
 routes = [
-    Route('/', home),
-    Route('/{object}', obj)
+    Route('/tree', tree),
+    Route('/object/{object}', obj),
+    Mount('/', app=StaticFiles(directory=SRV_HOME, html=True)),
 ]
 
 app = Starlette(debug=True, routes=routes, lifespan=lifespan)
